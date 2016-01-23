fd09e95b7a3613d818c6afd6320fc06e034d6050
artemylance <artemylance@gmail.com>
2016-01-23 08:35:53 +0300

    bower and other config

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-var http = require("http");
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+    var filePath = path.join(__dirname, 'storage.json');
+    var stat = fileSystem.statSync(filePath);
 
+    response.writeHead(200, {
+        'Content-Type': 'application/json',
+        'Content-Length': stat.size
+    });
+
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(response);
+})
+.listen(2000);
