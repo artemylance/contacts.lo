64bde47899bab04988045e584d47a317ba52c72b
artemylance <artemylance@gmail.com>
2016-03-15 00:47:15 +0300

    fix routing for static files at serverside

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require("http"),
-	url = require("url"),
-	router = require("./router"),
-	handlers = require("./handlers");
+    fs = require("fs"),
+  	url = require("url"),
+    path = require("path"),
+    mime = require("mime"),
+  	router = require("./router"),
+  	handlers = require("./handlers"),
+    port = process.argv[2] || 8888;
 
 var handle = {};
 handle["/"] = handlers.start;
@@ -11,16 +15,44 @@ handle["/update"] = handlers.update;
 handle["/remove"] = handlers.remove;
 handle["/full_calendar"] = handlers.full_calendar;
 
+function send404(response) {
+  response.writeHead(404, {"Content-type" : "text/plain"});
+  response.write("Error 404: resource not found");
+  response.end();
+}
+
+function sendPage(response, filePath, fileContents) {
+  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+  response.end(fileContents);
+}
+
 function start(route, handle) {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
+    var parsedUrl = url.parse(request.url),
+        pathname = parsedUrl.pathname,
+        filename = "." + pathname;
     // console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname, response);
+    fs.exists(filename, function(exists){
+      if(exists && filename != "./") {
+        console.log(filename);
+        fs.readFile(filename, function(err, data) {
+          if (err) {
+            console.log(err);
+            send404(response);
+          } else {
+            sendPage(response, filename, data);
+          }
+        });
+      } else {
+        route(handle, pathname, response);
+      }
+    });
+
   }
 
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  http.createServer(onRequest).listen(parseInt(port, 10));
+  console.log("Server has started on " + parseInt(port, 10) + " port.");
 }
 
 start(router.route, handle);
